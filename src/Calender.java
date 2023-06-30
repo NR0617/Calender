5ebe43ac3f9875b3af666a954fce34f208e111dc
DESKTOP-39CBMEP\User <nara20210617@gmail.com>
2023-06-30 16:36:15 +0900

    feat:업데이트 캘린더

--- src/Calender.java
+++ src/Calender.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Calender {
     private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
     public int getMaxDaysOfMonth(int month) {
         return MAX_DAYS[month - 1];
     }
+
     public void sampleCalender() {
         System.out.println(" 일  월   화  수  목   금  토");
         System.out.println("------------------------------");
@@ -14,17 +16,25 @@ public class Calender {
         System.out.println("22  23  24  25  26  27  28");
         System.out.println("29 30");
     }
+
     public static void main(String[] args) {
         System.out.println("Hello Calender!");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("달을 입력하세요");
-        int month = scanner.nextInt();
 
-        Calender cal = new Calender();
-        cal.sampleCalender();
+        System.out.println("반복 횟수를 입력하세요");
+        int repeat = scanner.nextInt();
 
-        System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+        for(int i = 0; i<repeat; i++) {
+            System.out.println("달을 입력하세요");
+
+            Calender cal = new Calender();
+//            cal.sampleCalender();
+            int month = scanner.nextInt();
+            System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+        }
+
+        System.out.println("Bye~");
         scanner.close();
     }
 }
