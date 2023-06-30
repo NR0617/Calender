524dcc3df43369771ca89a538b5a993680411429
DESKTOP-39CBMEP\User <nara20210617@gmail.com>
2023-06-30 16:12:33 +0900

    feat:인스턴스를 이용하여 입력한 월의 최대 일수 구하기

--- src/Calender.java
+++ src/Calender.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
 public class Calender {
-    public static void main(String[] args) {
-        System.out.println("Hello Calender!");
+    private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public int getMaxDaysOfMonth(int month) {
+        return MAX_DAYS[month - 1];
+    }
+    public void sampleCalender() {
         System.out.println(" 일  월   화  수  목   금  토");
         System.out.println("------------------------------");
         System.out.println(" 1   2   3   4   5   6   7");
@@ -10,13 +13,18 @@ public class Calender {
         System.out.println("15  16  17  18  19  20  21");
         System.out.println("22  23  24  25  26  27  28");
         System.out.println("29 30");
-
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello Calender!");
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("달을 입력하세요");
         int month = scanner.nextInt();
 
-        int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month -1]);
+        Calender cal = new Calender();
+        cal.sampleCalender();
+
+        System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+        scanner.close();
     }
 }
