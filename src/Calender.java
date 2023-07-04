4057bd4dce75699ef48327d064c984df3e9dbba4
DESKTOP-39CBMEP\User <nara20210617@gmail.com>
2023-07-04 10:02:29 +0900

    feat: 저장, 조회기능 완성

--- src/Calender.java
+++ src/Calender.java
@@ -1,16 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Calender {
     private static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     private static final int[] LEAP_MAX_DAYS = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
+    private static final String SAVE_FILE = "calendar.dat";
     private HashMap<Date, PlanItem> planMap;
 
     public Calender() {
         // 뭔가를 저장할 때 해시맵 또는 해시테이블 이용
         planMap = new HashMap<Date, PlanItem>();
+        File f = new File(SAVE_FILE);
+        if (!f.exists()) {
+            System.err.println("no file");
+            return ;
+        }
+        try {
+            Scanner s = new Scanner(f);
+            while (s.hasNext()) {
+                String line = s.nextLine();
+                String[] words = line.split(",");
+                String date = words[0];
+                String detail = words[1].replaceAll("\"", "");
+                System.out.println(date + ":" + detail);
+                PlanItem p = new PlanItem(date, detail);
+                planMap.put(p.getDate(), p);
+            }
+            s.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /*
@@ -18,10 +44,18 @@ public class Calender {
         @param plan
      */
     public void registerPlan(String strDate, String plan) {
-       // Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
-      //  System.out.println(date);
         PlanItem p = new PlanItem(strDate, plan);
         planMap.put(p.getDate(), p); // 일정을 저장
+
+        File f = new File(SAVE_FILE);
+        String item = p.saveString();
+        try {
+            FileWriter fw = new FileWriter(f, true);
+            fw.write(item);
+            fw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public PlanItem searchPlan(String strDate) {
